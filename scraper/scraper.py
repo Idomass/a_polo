ba6165b95f34673a8cdf4ff7ea673f4d44133f84
Adar Arbiv <mrmetrix4@gmail.com>
2023-02-15 15:28:19 +0200

    Added all json

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,7 @@
 from typing import List
 import requests
 from json import dump, load, loads
+from os import listdir
 
 API_KEY = "pub_172363ab3a9e0d9baad7f443feba04fc43dbc"
 SCRAPE_URL_TEMPLATE = "https://newsdata.io/api/1/news?apikey={api_key}&language=he&domain={source}"
@@ -11,7 +12,7 @@ def scrape_source(source: str) -> bool:
     rsp = requests.get(source_url)
     if not rsp.ok:
         return False
-    with open(f"scraping_data/{source}.json", 'w', encoding='utf-8') as scrape_data:
+    with open(f"scraping_temp_data/{source}.json", 'w', encoding='utf-8') as scrape_data:
         dump(loads(rsp.text)['results'], scrape_data)
     return True
 
@@ -24,6 +25,18 @@ def scrape_all_sources() -> None:
             print(f"* Scraped {source}")
         else:
             print(f"* Error scraping {source}")
+
+
+def unify_all_jsons():
+    with open("sources.json") as sources:
+        sources = load(sources).keys()
+    all_json = []
+    for source in sources:
+        with open(f"scraping_temp_data/{source}.json") as json_file:
+            all_json.extend(load(json_file))
+    with open(f"scraping_data/all.json", 'w') as json_file:
+        dump(all_json, json_file)
+
 
 # Those functions are responsible to convert a news site url to arcticles
 
