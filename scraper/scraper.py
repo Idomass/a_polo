e48ec3fbf6e494c89637d1bc636a1142c4d041c3
Ido Masubi <idomass@github.com>
2023-02-15 13:19:30 +0200

    Added empty scripts and venv

---
+++ scraper/scraper.py
@@ -0,0 +1,15 @@
+
+# Those functions are responsible to convert a news site url to arcticles
+
+def map_article(url: str) -> List(str):
+    '''
+    This function gets a url to a news site then returns a list of
+    all the urls of articles currently in the sites.
+    '''
+    pass
+
+def is_article(url: str) -> bool:
+    '''
+    This function checks if certain url contains an arcticle
+    '''
+    pass
