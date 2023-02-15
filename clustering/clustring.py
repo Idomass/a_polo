6e593fdd73e1d3bea18482ee35e05f243b6d078a
= <ori195@gmail.com>
2023-02-15 18:20:00 +0200

    Showing somthing

--- clustering/clustring.py
+++ clustering/clustring.py
@@ -2,13 +2,17 @@ import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 import numpy as np
+from sklearn.decomposition import PCA
 from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt
+
 
 with open('all.json', encoding = "utf-8") as f:
 	json_acceses = json.load(f)
 
-	contents = [access['description'] for access in json_acceses['results'] if access['description'] != None]
-
+	# breakpoint()
+	contents = [access['english_content'] for access in json_acceses if access['english_content'] != None]
+	subjects = [access['english_title'] for access in json_acceses if access['english_content'] != None]
 
 	# print(contents)d
 
@@ -21,19 +25,31 @@ with open('all.json', encoding = "utf-8") as f:
 	# arr = np.empty(shape = [len(contents), len(features)])
 	
 
-	# pca = TruncatedSVD(n_components=2)
-	# pca.fit(fitness_scores)
+	pca = PCA()
+	x = pca.fit_transform(fitness_scores.toarray())
 
-	# breakpoint()
+	print(pca)
 
-	# print(pca)
+	plt.scatter(x[:,0], x[:,1])
+	plt.xlabel('PC1')
+	plt.ylabel('PC2')
+	plt.show()
+
+	breakpoint()
+
 
 	# for i in fitness_scores:
 	# 	print()
 
-	kmeans = KMeans(n_clusters=6, random_state=0, n_init="auto").fit(X)
+	kmeans = KMeans(n_clusters=10, random_state=0, n_init="auto").fit(fitness_scores)
 
+	print(kmeans.labels_)
 
+	ordered = [[] for i in range(0,10)]
 
+	for i,label in enumerate(kmeans.labels_):
+		ordered[label].append(subjects[i])
 
-	
+	for cluster in ordered:
+		print("Cluester" + str(cluster))
+		print("\n")
